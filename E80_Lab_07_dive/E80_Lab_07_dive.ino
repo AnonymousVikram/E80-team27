0cf4765c993b43b72eb92ca8bd227ca867bbf0ba
Vikram Krishna <vkrishna@hmc.edu>
2024-03-20 14:14:13 -0700

    initialized

--- E80_Lab_07_dive/E80_Lab_07_dive.ino
+++ E80_Lab_07_dive/E80_Lab_07_dive.ino
@@ -3,29 +3,30 @@ E80 Lab 7 Dive Activity Code
 Authors:
     Omar Aleman (oaleman@g.hmc.edu) '21 (contributed 2019)
     Wilson Ives (wives@g.hmc.edu) '20 (contributed in 2018)
-    Christopher McElroy (cmcelroy@g.hmc.edu) '19 (contributed in 2017)  
+    Christopher McElroy (cmcelroy@g.hmc.edu) '19 (contributed in 2017)
     Josephine Wong (jowong@hmc.edu) '18 (contributed in 2016)
-    Apoorva Sharma (asharma@hmc.edu) '17 (contributed in 2016)                    
+    Apoorva Sharma (asharma@hmc.edu) '17 (contributed in 2016)
 */
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 
+#include <ADCSampler.h>
+#include <ButtonSampler.h> // A template of a data source library
+#include <DepthControl.h>
+#include <ErrorFlagSampler.h>
+#include <Logger.h>
+#include <MotorDriver.h>
 #include <Pinouts.h>
-#include <TimingOffsets.h>
+#include <Printer.h>
 #include <SensorGPS.h>
 #include <SensorIMU.h>
+#include <TimingOffsets.h>
 #include <XYStateEstimator.h>
 #include <ZStateEstimator.h>
-#include <ADCSampler.h>
-#include <ErrorFlagSampler.h>
-#include <ButtonSampler.h> // A template of a data source library
-#include <MotorDriver.h>
-#include <Logger.h>
-#include <Printer.h>
-#include <DepthControl.h>
+
 #define UartSerial Serial1
 #include <GPSLockLED.h>
 
@@ -48,12 +49,12 @@ GPSLockLED led;
 // loop start recorder
 int loopStartTime;
 int currentTime;
-volatile bool EF_States[NUM_FLAGS] = {1,1,1};
+volatile bool EF_States[NUM_FLAGS] = {1, 1, 1};
 
 ////////////////////////* Setup *////////////////////////////////
 
 void setup() {
-  
+
   logger.include(&imu);
   logger.include(&gps);
   logger.include(&xy_state_estimator);
@@ -74,83 +75,84 @@ void setup() {
   motor_driver.init();
   led.init();
 
-  int diveDelay = 2000; // how long robot will stay at depth waypoint before continuing (ms)
+  int diveDelay =
+      2000; // how long robot will stay at depth waypoint before continuing (ms)
 
   const int num_depth_waypoints = 2;
-  double depth_waypoints [] = { 0.5, 1 };  // listed as z0,z1,... etc.
+  double depth_waypoints[] = {0.5, 1}; // listed as z0,z1,... etc.
   depth_control.init(num_depth_waypoints, depth_waypoints, diveDelay);
-  
-  xy_state_estimator.init(); 
+
+  xy_state_estimator.init();
   z_state_estimator.init();
 
-  printer.printMessage("Starting main loop",10);
+  printer.printMessage("Starting main loop", 10);
   loopStartTime = millis();
-  printer.lastExecutionTime            = loopStartTime - LOOP_PERIOD + PRINTER_LOOP_OFFSET ;
-  imu.lastExecutionTime                = loopStartTime - LOOP_PERIOD + IMU_LOOP_OFFSET;
-  adc.lastExecutionTime                = loopStartTime - LOOP_PERIOD + ADC_LOOP_OFFSET;
-  ef.lastExecutionTime                 = loopStartTime - LOOP_PERIOD + ERROR_FLAG_LOOP_OFFSET;
-  button_sampler.lastExecutionTime     = loopStartTime - LOOP_PERIOD + BUTTON_LOOP_OFFSET;
-  xy_state_estimator.lastExecutionTime = loopStartTime - LOOP_PERIOD + XY_STATE_ESTIMATOR_LOOP_OFFSET;
-  z_state_estimator.lastExecutionTime  = loopStartTime - LOOP_PERIOD + Z_STATE_ESTIMATOR_LOOP_OFFSET;
-  depth_control.lastExecutionTime      = loopStartTime - LOOP_PERIOD + DEPTH_CONTROL_LOOP_OFFSET;
-  logger.lastExecutionTime             = loopStartTime - LOOP_PERIOD + LOGGER_LOOP_OFFSET;
-
+  printer.lastExecutionTime = loopStartTime - LOOP_PERIOD + PRINTER_LOOP_OFFSET;
+  imu.lastExecutionTime = loopStartTime - LOOP_PERIOD + IMU_LOOP_OFFSET;
+  adc.lastExecutionTime = loopStartTime - LOOP_PERIOD + ADC_LOOP_OFFSET;
+  ef.lastExecutionTime = loopStartTime - LOOP_PERIOD + ERROR_FLAG_LOOP_OFFSET;
+  button_sampler.lastExecutionTime =
+      loopStartTime - LOOP_PERIOD + BUTTON_LOOP_OFFSET;
+  xy_state_estimator.lastExecutionTime =
+      loopStartTime - LOOP_PERIOD + XY_STATE_ESTIMATOR_LOOP_OFFSET;
+  z_state_estimator.lastExecutionTime =
+      loopStartTime - LOOP_PERIOD + Z_STATE_ESTIMATOR_LOOP_OFFSET;
+  depth_control.lastExecutionTime =
+      loopStartTime - LOOP_PERIOD + DEPTH_CONTROL_LOOP_OFFSET;
+  logger.lastExecutionTime = loopStartTime - LOOP_PERIOD + LOGGER_LOOP_OFFSET;
 }
-
-
 
 //////////////////////////////* Loop */////////////////////////
 
 void loop() {
-  currentTime=millis();
-    
-  if ( currentTime-printer.lastExecutionTime > LOOP_PERIOD ) {
+  currentTime = millis();
+
+  if (currentTime - printer.lastExecutionTime > LOOP_PERIOD) {
     printer.lastExecutionTime = currentTime;
-    printer.printValue(0,adc.printSample());
-    printer.printValue(1,ef.printStates());
-    printer.printValue(2,logger.printState());
-    printer.printValue(3,gps.printState());   
-    printer.printValue(4,xy_state_estimator.printState());  
-    printer.printValue(5,z_state_estimator.printState());  
-    printer.printValue(6,depth_control.printWaypointUpdate());
-    printer.printValue(7,depth_control.printString());
-    printer.printValue(8,motor_driver.printState());
-    printer.printValue(9,imu.printRollPitchHeading());        
-    printer.printValue(10,imu.printAccels());
-    printer.printToSerial();  // To stop printing, just comment this line out
+    printer.printValue(0, adc.printSample());
+    printer.printValue(1, ef.printStates());
+    printer.printValue(2, logger.printState());
+    printer.printValue(3, gps.printState());
+    printer.printValue(4, xy_state_estimator.printState());
+    printer.printValue(5, z_state_estimator.printState());
+    printer.printValue(6, depth_control.printWaypointUpdate());
+    printer.printValue(7, depth_control.printString());
+    printer.printValue(8, motor_driver.printState());
+    printer.printValue(9, imu.printRollPitchHeading());
+    printer.printValue(10, imu.printAccels());
+    printer.printToSerial(); // To stop printing, just comment this line out
   }
 
   /* ROBOT CONTROL Finite State Machine */
-  if ( currentTime-depth_control.lastExecutionTime > LOOP_PERIOD ) {
+  if (currentTime - depth_control.lastExecutionTime > LOOP_PERIOD) {
     depth_control.lastExecutionTime = currentTime;
-    if ( depth_control.diveState ) {      // DIVE STATE //
+    if (depth_control.diveState) { // DIVE STATE //
       depth_control.complete = false;
-      if ( !depth_control.atDepth ) {
+      if (!depth_control.atDepth) {
         depth_control.dive(&z_state_estimator.state, currentTime);
-      }
-      else {
-        depth_control.diveState = false; 
+      } else {
+        depth_control.diveState = false;
         depth_control.surfaceState = true;
       }
-      motor_driver.drive(0,0,depth_control.uV);
+      motor_driver.drive(0, 0, depth_control.uV);
     }
-    if ( depth_control.surfaceState ) {     // SURFACE STATE //
-      if ( !depth_control.atSurface ) { 
+    if (depth_control.surfaceState) { // SURFACE STATE //
+      if (!depth_control.atSurface) {
         depth_control.surface(&z_state_estimator.state);
+      } else if (depth_control.complete) {
+        delete[] depth_control
+            .wayPoints; // destroy depth waypoint array from the Heap
       }
-      else if ( depth_control.complete ) { 
-        delete[] depth_control.wayPoints;   // destroy depth waypoint array from the Heap
-      }
-      motor_driver.drive(0,0,depth_control.uV);
+      motor_driver.drive(0, 0, depth_control.uV);
     }
-  }
-  
-  if ( currentTime-adc.lastExecutionTime > LOOP_PERIOD ) {
-    adc.lastExecutionTime = currentTime;
-    adc.updateSample(); 
   }
 
-  if ( currentTime-ef.lastExecutionTime > LOOP_PERIOD ) {
+  if (currentTime - adc.lastExecutionTime > LOOP_PERIOD) {
+    adc.lastExecutionTime = currentTime;
+    adc.updateSample();
+  }
+
+  if (currentTime - ef.lastExecutionTime > LOOP_PERIOD) {
     ef.lastExecutionTime = currentTime;
     attachInterrupt(digitalPinToInterrupt(ERROR_FLAG_A), EFA_Detected, LOW);
     attachInterrupt(digitalPinToInterrupt(ERROR_FLAG_B), EFB_Detected, LOW);
@@ -159,53 +161,49 @@ void loop() {
     detachInterrupt(digitalPinToInterrupt(ERROR_FLAG_A));
     detachInterrupt(digitalPinToInterrupt(ERROR_FLAG_B));
     detachInterrupt(digitalPinToInterrupt(ERROR_FLAG_C));
-    ef.updateStates(EF_States[0],EF_States[1],EF_States[2]);
+    ef.updateStates(EF_States[0], EF_States[1], EF_States[2]);
     EF_States[0] = 1;
     EF_States[1] = 1;
     EF_States[2] = 1;
   }
 
-  if ( currentTime-button_sampler.lastExecutionTime > LOOP_PERIOD ) {
+  if (currentTime - button_sampler.lastExecutionTime > LOOP_PERIOD) {
     button_sampler.lastExecutionTime = currentTime;
     button_sampler.updateState();
   }
 
-  if ( currentTime-imu.lastExecutionTime > LOOP_PERIOD ) {
+  if (currentTime - imu.lastExecutionTime > LOOP_PERIOD) {
     imu.lastExecutionTime = currentTime;
-    imu.read();     // blocking I2C calls
+    imu.read(); // blocking I2C calls
   }
- 
-  gps.read(&GPS); // blocking UART calls, need to check for UART data every cycle
 
-  if ( currentTime-xy_state_estimator.lastExecutionTime > LOOP_PERIOD ) {
+  gps.read(
+      &GPS); // blocking UART calls, need to check for UART data every cycle
+
+  if (currentTime - xy_state_estimator.lastExecutionTime > LOOP_PERIOD) {
     xy_state_estimator.lastExecutionTime = currentTime;
     xy_state_estimator.updateState(&imu.state, &gps.state);
   }
 
-  if ( currentTime-z_state_estimator.lastExecutionTime > LOOP_PERIOD ) {
+  if (currentTime - z_state_estimator.lastExecutionTime > LOOP_PERIOD) {
     z_state_estimator.lastExecutionTime = currentTime;
     z_state_estimator.updateState(analogRead(PRESSURE_PIN));
   }
-  
-  if ( currentTime-led.lastExecutionTime > LOOP_PERIOD ) {
+
+  if (currentTime - led.lastExecutionTime > LOOP_PERIOD) {
     led.lastExecutionTime = currentTime;
     led.flashLED(&gps.state);
   }
 
-  if ( currentTime- logger.lastExecutionTime > LOOP_PERIOD && logger.keepLogging ) {
+  if (currentTime - logger.lastExecutionTime > LOOP_PERIOD &&
+      logger.keepLogging) {
     logger.lastExecutionTime = currentTime;
     logger.log();
   }
 }
 
-void EFA_Detected(void){
-  EF_States[0] = 0;
-}
+void EFA_Detected(void) { EF_States[0] = 0; }
 
-void EFB_Detected(void){
-  EF_States[1] = 0;
-}
+void EFB_Detected(void) { EF_States[1] = 0; }
 
-void EFC_Detected(void){
-  EF_States[2] = 0;
-}
+void EFC_Detected(void) { EF_States[2] = 0; }
